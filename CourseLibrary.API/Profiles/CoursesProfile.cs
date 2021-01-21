e4a22572470d2f811ce17ada8d5e4cc7e17208dc
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-01-21 14:11:45 +0200

    Added new restfull api

---
+++ CourseLibrary.API/Profiles/CoursesProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Entities;
+using CourseLibrary.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Profiles
+{
+    public class CoursesProfile : Profile
+    {
+        public CoursesProfile()
+        {
+            CreateMap<Course, CourseDto>();
+            CreateMap<CourseForCreationDto, Course>();
+            CreateMap<CourseForUpdateDto, Course>();
+            CreateMap<Course, CourseForUpdateDto>();
+        }
+    }
+}
